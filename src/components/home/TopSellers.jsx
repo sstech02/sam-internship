7487d5a9c21a21173b4cba8b1830d06e4365c3fe
sstech02 <samuelsch002@gmail.com>
2026-04-08 11:38:13 -0500

    Top Sellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import Skeleton from "../UI/Skeleton";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Skeleton from '../UI/Skeleton'
 import axios from 'axios'
 
 const TopSellers = () => {
-  const [items, setItems] = useState([]);
-  const [dataIsLoaded, setDataIsLoaded] = useState(false);
+  const [items, setItems] = useState([])
+  const [dataIsLoaded, setDataIsLoaded] = useState(false)
 
   useEffect(() => {
     axios
@@ -23,25 +23,34 @@ const TopSellers = () => {
 
   if (!dataIsLoaded) {
     return (
-      <section id="section-popular" className="pb-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="text-center">
+      <section id='section-popular' className='pb-5'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-12'>
+              <div className='text-center'>
                 <h2>Top Sellers</h2>
-                <div className="small-border bg-color-2"></div>
+                <div className='small-border bg-color-2'></div>
               </div>
             </div>
-            <div className="col-md-12">
-              <ol className="author_list">
+            <div className='col-md-12'>
+              <ol className='author_list'>
                 {new Array(12).fill(0).map((_, index) => (
-                  <li key={index} className="skeleton-author-item">
-                    <div className="author_list_pp skeleton-avatar">
-                      <Skeleton width="50px" height="50px" borderRadius="50%" />
+                  <li key={index} className='skeleton-author-item'>
+                    <div className='author_list_pp skeleton-avatar'>
+                      <Skeleton width='50px' height='50px' borderRadius='50%' />
                     </div>
-                    <div className="author_list_info">
-                      <Skeleton width="150px" height="18px" borderRadius="5px" style={{ marginBottom: "8px" }} />
-                      <Skeleton width="100px" height="16px" borderRadius="5px" />
+                    <div className='author_list_info'>
+                      <Skeleton
+                        width='150px'
+                        height='18px'
+                        borderRadius='5px'
+                        style={{ marginBottom: '8px' }}
+                      />
+                      <Skeleton
+                        width='100px'
+                        height='16px'
+                        borderRadius='5px'
+                      />
                     </div>
                   </li>
                 ))}
@@ -50,35 +59,37 @@ const TopSellers = () => {
           </div>
         </div>
       </section>
-    );
+    )
   }
 
   return (
-    <section id="section-popular" className="pb-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
+    <section id='section-popular' className='pb-5'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='text-center'>
               <h2>Top Sellers</h2>
-              <div className="small-border bg-color-2"></div>
+              <div className='small-border bg-color-2'></div>
             </div>
           </div>
-          <div className="col-md-12">
-            <ol className="author_list">
-              {items.map((item) => (
+          <div className='col-md-12'>
+            <ol className='author_list'>
+              {items.map(item => (
                 <li key={item.id}>
-                  <div className="author_list_pp">
+                  <div className='author_list_pp'>
                     <Link to={`/author/${item.authorId}`}>
                       <img
-                        className="lazy pp-author"
+                        className='lazy pp-author'
                         src={item.authorImage}
-                        alt=""
+                        alt=''
                       />
-                      <i className="fa fa-check"></i>
+                      <i className='fa fa-check'></i>
                     </Link>
                   </div>
-                  <div className="author_list_info">
-                    <Link to={`/author/${item.authorId}`}>{item.authorName}</Link>
+                  <div className='author_list_info'>
+                    <Link to={`/author/${item.authorId}`}>
+                      {item.authorName}
+                    </Link>
                     <span>{item.totalVolume} ETH</span>
                   </div>
                 </li>
@@ -88,7 +99,7 @@ const TopSellers = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default TopSellers;
+export default TopSellers
