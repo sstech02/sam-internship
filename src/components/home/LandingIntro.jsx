c66593e1180dd8480ee047f5c8571059c3045b9d
sstech02 <samuelsch002@gmail.com>
2026-04-15 09:15:28 -0500

    4/15/2026

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -1,53 +1,95 @@
-import React from "react";
+import React from 'react'
 
 const LandingIntro = () => {
   return (
-    <section id="section-intro" className="no-top no-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_wallet"></i>
-              <div className="text">
-                <h4 className="">Set up your wallet</h4>
-                <p>
+    <section id='section-intro' className='no-top no-bottom'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-4 col-md-6 mb-sm-30'>
+            <div className='feature-box f-boxed style-3'>
+              <i
+                className='wow fadeInUp bg-color-2 i-boxed icon_wallet'
+                style={{ visibility: 'visible', animationDelay: '1.25s' }}
+                data-aos='fade-Up'
+              ></i>
+              <div className='text'>
+                <h4
+                  className='wow fadeInUp'
+                  data-aos='fade-Up'
+                  style={{ visibility: 'visible', animationDelay: '1.25s' }}
+                >
+                  Set up your wallet
+                </h4>
+                <p
+                  className='wow fadeInUp'
+                  data-aos='fade-Up'
+                  style={{ visibility: 'visible', animationDelay: '0.25s' }}
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem.
+                  accusantium doloremque laudantium, totam rem. lorem
                 </p>
               </div>
-              <i className="wm icon_wallet"></i>
+              <i className='wm icon_wallet'></i>
             </div>
           </div>
-          <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
-              <div className="text">
-                <h4 className="">Add your NFT's</h4>
-                <p>
+          <div className='col-lg-4 col-md-6 mb-sm-30'>
+            <div className='feature-box f-boxed style-3'>
+              <i
+                className='wow fadeInUp bg-color-2 i-boxed icon_wallet'
+                style={{ visibility: 'visible', animationDelay: '1.25s' }}
+                data-aos='fade-Up'
+              ></i>
+              <div className='text'>
+                <h4
+                  className='wow fadeInUp'
+                  data-aos='fade-Up'
+                  style={{ visibility: 'visible', animationDelay: '1.25s' }}
+                >
+                  Add your NFT's
+                </h4>
+                <p
+                  className='wow fadeInUp'
+                  data-aos='fade-Up'
+                  style={{ visibility: 'visible', animationDelay: '0.25s' }}
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem.
+                  accusantium doloremque laudantium, totam rem. lorem
                 </p>
               </div>
-              <i className="wm icon_cloud-upload_alt"></i>
+              <i className='wm icon_cloud-upload_alt'></i>
             </div>
           </div>
-          <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_tags_alt"></i>
-              <div className="text">
-                <h4 className="">Sell your NFT's</h4>
-                <p>
+          <div className='col-lg-4 col-md-6 mb-sm-30'>
+            <div className='feature-box f-boxed style-3'>
+              <i
+                className='wow fadeInUp bg-color-2 i-boxed icon_wallet'
+                style={{ visibility: 'visible', animationDelay: '1.25s' }}
+                data-aos='fade-Up'
+              ></i>
+              <div className='text'>
+                <h4
+                  className='wow fadeInUp'
+                  data-aos='fade-Up'
+                  style={{ visibility: 'visible', animationDelay: '1.25s' }}
+                >
+                  Sell your NFT's
+                </h4>
+                <p
+                  className='wow fadeInUp'
+                  data-aos='fade-Up'
+                  style={{ visibility: 'visible', animationDelay: '0.25s' }}
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem.
+                  accusantium doloremque laudantium, totam rem. lorem
                 </p>
               </div>
-              <i className="wm icon_tags_alt"></i>
+              <i className='wm icon_tags_alt'></i>
             </div>
           </div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default LandingIntro;
+export default LandingIntro
--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -68,7 +68,13 @@ const TopSellers = () => {
         <div className='row'>
           <div className='col-lg-12'>
             <div className='text-center'>
-              <h2>Top Sellers</h2>
+              <h2
+                className='wow fadeInUp'
+                data-aos='fade-Up'
+                style={{ visibility: 'visible', animationDelay: '1s' }}
+              >
+                Top Sellers
+              </h2>
               <div className='small-border bg-color-2'></div>
             </div>
           </div>
