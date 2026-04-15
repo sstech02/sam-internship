57caded0718f6a59dd235a285257a4300455e055
sstech02 <samuelsch002@gmail.com>
2026-04-15 12:03:18 -0500

    Vercel Debug 1

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -107,23 +107,7 @@ const ExploreItems = () => {
             </div>
             <Timer expiryDate={item.expiryDate} />
             <div className='nft__item_wrap'>
-              <div className='nft__item_extra'>
-                <div className='nft__item_buttons'>
-                  <button>Buy Now</button>
-                  <div className='nft__item_share'>
-                    <h4>Share</h4>
-                    <a href='' target='_blank' rel='noreferrer'>
-                      <i className='fa fa-facebook fa-lg'></i>
-                    </a>
-                    <a href='' target='_blank' rel='noreferrer'>
-                      <i className='fa fa-twitter fa-lg'></i>
-                    </a>
-                    <a href=''>
-                      <i className='fa fa-envelope fa-lg'></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <div className='nft__item_extra'></div>
               <Link to={`/item-details/${item.nftId}`}>
                 <img
                   src={item.nftImage}
